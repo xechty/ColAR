93541bd024e707eb2ccfb2991b81d58118afcc0d
Xechty <mp12694@my.bristol.ac.uk>
2016-04-16 21:48:35 +0100

    working

--- Assets/GetCameraImage.cs
+++ Assets/GetCameraImage.cs
@@ -11,49 +11,41 @@ public class GetCameraImage : MonoBehaviour
     public Material newMaterialRef;
     void Start()
     {
-        VuforiaBehaviour vuforiaBehaviour = (VuforiaBehaviour)FindObjectOfType(typeof(VuforiaBehaviour));
-        if (vuforiaBehaviour)
-        {
-            vuforiaBehaviour.RegisterTrackablesUpdatedCallback(OnTrackablesUpdated);
-        }
+        
     }
-    public void OnTrackablesUpdated()
+    public void Update()
     {
         if (!m_RegisteredFormat)
         {
             CameraDevice.Instance.SetFrameFormat(m_PixelFormat, true);
             m_RegisteredFormat = true;
         }
-        if (m_LogInfo)
+        CameraDevice cam = CameraDevice.Instance;
+        Image image = cam.GetCameraImage(m_PixelFormat);
+        if (image == null)
         {
-            CameraDevice cam = CameraDevice.Instance;
-            Image image = cam.GetCameraImage(m_PixelFormat);
-            if (image == null)
-            {
-                Debug.Log(m_PixelFormat + " image is not available yet");
-            }
-            else
-            {
-                string s = m_PixelFormat + " image: \n";
-                s += "  size: " + image.Width + "x" + image.Height + "\n";
-                s += "  bufferSize: " + image.BufferWidth + "x" + image.BufferHeight + "\n";
-                s += "  stride: " + image.Stride;
-                Debug.Log(s);
-                m_LogInfo = false;
-                Texture2D tex = new Texture2D(2, 2);
-                //tex.LoadImage(image.Pixels);
-                //tex.Apply();
-                image.CopyToTexture(tex);
-                newMaterialRef.mainTexture = tex;
-                tex.Apply();
+            Debug.Log(m_PixelFormat + " image is not available yet");
+        }
+        else
+        {
+            string s = m_PixelFormat + " image: \n";
+            s += "  size: " + image.Width + "x" + image.Height + "\n";
+            s += "  bufferSize: " + image.BufferWidth + "x" + image.BufferHeight + "\n";
+            s += "  stride: " + image.Stride;
+            Debug.Log(s);
+            m_LogInfo = false;
+            Texture2D tex = new Texture2D(2, 2);
+            //tex.LoadImage(image.Pixels);
+            //tex.Apply();
+            image.CopyToTexture(tex);
+            newMaterialRef.mainTexture = tex;
+            tex.Apply();
 
-                // Encode texture into PNG
-                byte[] bytes = tex.EncodeToPNG();
+            // Encode texture into PNG
+            byte[] bytes = tex.EncodeToPNG();
 
-                // For testing purposes, also write to a file in the project folder
-                File.WriteAllBytes(Application.persistentDataPath + "/../SavedScreen.png", bytes);
-
-            }
+            // For testing purposes, also write to a file in the project folder
+            File.WriteAllBytes(Application.persistentDataPath + "/../SavedScreen.png", bytes);
 
         }
     }
