740f32743a8c315622c0c9de7db92c41bf788094
Xechty <mp12694@my.bristol.ac.uk>
2016-04-22 17:03:54 +0100

    animation runs in unity
    does not run in the app

---
+++ Assets/play_animation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_animation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.animation.play();
+	
+	}
+}
